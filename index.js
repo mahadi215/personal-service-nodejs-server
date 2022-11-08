2838531f26adf889622bda9be5cf8f22a0dfd84f
mahadi215 <mahadihasanbd215@gmail.com>
2022-11-08 19:23:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x4kttba.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const userCollection = client.db('simpledb').collection('users');
+        // const user = {email:"nahadi@gmail.com", name:"nahadi"};
+        // const result = await userCollection.insertOne(user);
+        // console.log(result);
+    }
+    finally{
+
+    }
+
+    
+}
+run().catch(err => console.log(err));
+
+
+
+app.get('/', (req,res) => {
+    res.send('hello from mongo server')
+});
+
+app.listen(port, ()=>{
+    console.log(`listening port ${port}`);
+})
